456306d2acd0e86271c7c95e6875c531ef838482
Christopher Dorst <chris@dorst.email>
2017-12-27 11:09:08 -0600

    Adding Any() function

---
+++ Common.Functions.CheckNullableEnumerationForAnyElements/NullableEnumerationAny.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Common.Functions.CheckNullableEnumerationForAnyElements
+{
+    public static class NullableEnumerationAny
+    {
+        public static bool Any<T>(IEnumerable<T> enumeration)
+            => enumeration?.Any() ?? false;
+    }
+}
